ebbffd633182033783ab06f0df18baa827e3a416
snowtoslow <snowtoslow@mail.ru>
2019-10-09 18:28:47 +0300

    change parameters name; change methods name;

--- lab3/src/com/snowtoslow/StringParser/StringParser.java
+++ lab3/src/com/snowtoslow/StringParser/StringParser.java
@@ -3,27 +3,27 @@ package com.snowtoslow.StringParser;
 
 public class StringParser {
 
-    private static int count = 0;
-    public void nrOfLetters(String smth) {
-        for (int i = 0; i < smth.length(); i++) {
+    private int count = 0;
+    public void getNrOfLetters(String text) {
+        for (int i = 0; i < text.length(); i++) {
             count = i;
         }
         System.out.println("Nr of letters in text:" + count);
     }
 
-    public void nrOfSEntences(String smth) {
-        int nrOfSentences = smth.split("[!?.]+").length;
+    public void getNrOfSEntences(String text) {
+        int nrOfSentences = text.split("[!?.]+").length;
         System.out.println("Nr of sentences is :" + nrOfSentences);
     }
 
-    public void nrOfConsAndWovels(String smth) {
-        int nrOfWovels = smth.split("[aeiouAEIOU]").length;
-        int nrOfConsonants = smth.split("[a-zA-Z&&[^aeiouAEIOU]]").length;
+    public void getNrOfConsAndWovels(String text) {
+        int nrOfWovels = text.split("[aeiouAEIOU]").length;
+        int nrOfConsonants = text.split("[a-zA-Z&&[^aeiouAEIOU]]").length;
         System.out.println("Nr of Vowels is: " + nrOfWovels + " and " + "Nr of Consonants is: " + nrOfConsonants);
     }
 
-    public void nrOfWords(String smth) {
-        int words = smth.split("[\\s]").length;
+    public void getNrOfWords(String text) {
+        int words = text.split("[\\s]").length;
         System.out.println("Nr of words:" + words);
     }
 }
