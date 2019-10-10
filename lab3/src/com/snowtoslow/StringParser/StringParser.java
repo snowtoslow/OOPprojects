0d40310d864e36ff2cd8c3b45983b8a0f0948a74
snowtoslow <snowtoslow@mail.ru>
2019-10-10 18:13:25 +0300

    add method count top5 and integrate values in a hashmap

--- lab3/src/com/snowtoslow/StringParser/StringParser.java
+++ lab3/src/com/snowtoslow/StringParser/StringParser.java
@@ -1,6 +1,8 @@
 package com.snowtoslow.StringParser;
 
 
+import java.util.*;
+
 public class StringParser {
 
     private int count = 0;
@@ -25,6 +27,38 @@ public class StringParser {
     public void getNrOfWords(String text) {
         int words = text.split("[\\s]").length;
         System.out.println("Nr of words:" + words);
+    }
+
+
+    public void countTop5(String text){
+        String[] words=text.split(" ");  //Split the word from String
+        int wrc=1;    //Variable for getting Repeated word count
+
+        //ArrayList<Integer> newArray = new ArrayList<Integer>();
+        HashMap<String, Integer> placeOfPastrare = new HashMap<>();
+        ArrayList<Integer> placeOfSorting = new ArrayList<Integer>();
+
+
+        for(int i=0;i<words.length;i++) //Outer loop for Comparison
+        {
+            for(int j=i+1;j<words.length;j++) //Inner loop for Comparison
+            {
+
+                if(words[i].equals(words[j]))  //Checking for both strings are equal
+                {
+                    wrc=wrc+1;    //if equal increment the count
+                    words[j]="0"; //Replace repeated words by zero
+                }
+            }
+            if(words[i]!="0")
+                //System.out.println(words[i]+"----->"+wrc); //Printing the word along with count
+                placeOfPastrare.put(words[i],wrc);
+            wrc=1;
+
+        }
+        System.out.println(placeOfPastrare);
+
+
     }
 }
 
